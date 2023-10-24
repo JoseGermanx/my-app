e8b5f2e11f6686c00acfe76fdd651a3e0531cfa6
Jose Germán Martínez <jgermanmm@gmail.com>
2023-10-24 18:32:28 -0300

    first commit

---
+++ src/components/button.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const Button = () => {
+  return (
+    
+        <button onClick={() => alert("Hola mundo")}>Click me</button>
+    
+  )
+}
+
+export default Button
---
+++ src/components/parrafo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "./button";
+
+const Parrafo = () => {
+  return (
+    <>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim iure
+      voluptatem, minus expedita eaque esse fugit hic nisi facere perspiciatis
+      pariatur aut architecto magnam harum similique maxime molestiae nam
+      deserunt?
+    </p>
+    <Button />
+    </>
+  );
+};
+
+export default Parrafo;
