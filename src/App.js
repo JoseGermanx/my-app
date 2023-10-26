0fa0b1321d405af41b676eb914fe4a1aa1c725e8
Jose Germán Martínez <jgermanmm@gmail.com>
2023-10-26 16:51:09 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,55 +1,39 @@
 import React, { useEffect, useState } from "react";  
 import "./App.css";
 import PersonCard from "./components/PersonCard";
-import EjemploEffect from "./components/ejemploEffect";
 
 function App() {
-  const personList = [
-    { id:"01", name: "Jose", username: "jose45", email: "jose@email.com" },
-    { id:"02", name: "Luis", username: "lucho45", email: "luis@email.com" },
-    { id:"03", name: "Cata", username: "cate10", email: "cata@email.com" },
-    { id:"04", name: "Claudio", username: "claudio643", email: "claudio@email.com" },
-  ];  
 
   const [personas, setPersonas] = useState([]);
 
   const mostrarPersonas = () => {
-    setPersonas(personList);
+    setPersonas();
   };
 
   const deletePersonas = () => {
     setPersonas([]);
   };
 
-  const setPerson = () => {
-    
-    const person = {
-      id: "05",
-      name: "Carlos",
-      username: "Carlitos",
-      email: "jose@email.com"
-    }
-    personList.push(person);
-    setPersonas(personList)
-  }
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) => setPersonas(data));
+  }, []);
 
   return (
     <div className="App">
       <h1>Lista de personas</h1>
       <button onClick={mostrarPersonas}>Mostrar lista</button>
       <button onClick={deletePersonas}>Borrar lista</button>
-      <button onClick={setPerson}>Agregar persona</button>
 
-      {/* {personas.map((person) => (
+      {personas.map((person) => (
         <PersonCard
           key={person.id}
           nombrePersona={person.name}
           NombreUsuario={person.username}
           emailUsuario={person.email}
         />
-      ))} */}
-
-      <EjemploEffect />
+      ))}
     </div>
   );
 }
